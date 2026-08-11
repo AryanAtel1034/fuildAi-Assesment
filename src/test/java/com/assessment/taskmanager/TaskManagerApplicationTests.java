d060dd87e77f3b8c0cea811892bb60fa2b1a68a7
Aryan Atel <115566468+AryanAtel1034@users.noreply.github.com>
2026-08-11 15:42:45 +0530

    fixed build issue

--- src/test/java/com/assessment/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/assessment/taskmanager/TaskManagerApplicationTests.java
@@ -1,12 +1,16 @@
+```java
 package com.assessment.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskManagerApplicationTests {
 
     @Test
     void contextLoads() {
     }
 }
+```
